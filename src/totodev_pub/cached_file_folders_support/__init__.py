26fc937f4ef6d2bd127a8a45079520e8311d35c2
Dave Foster <dave@tomorrowtoday.com>
2026-07-07 13:30:32 -0700

    Breaking change that nobody will care about.  I removed the SimpleCacheORM and related classes completely.  Nobody used them, nobody wanted them.

--- src/totodev_pub/cached_file_folders_support/__init__.py
+++ src/totodev_pub/cached_file_folders_support/__init__.py
@@ -7,7 +7,6 @@ from .cache_operations_protocol import CacheOperations
 from .resync_sweep import AsyncSyncSession
 from .async_operation_handlers import AsyncUpsertOperation, AsyncDeleteOperation
 from .resync_orchestrator import ResyncOrchestrator, FileSnapshot
-# Avoid eager import of SimpleCacheORM to prevent circular imports
 
 __all__ = [
     "ChangeType",
@@ -18,30 +17,4 @@ __all__ = [
     "AsyncDeleteOperation",
     "ResyncOrchestrator",
     "FileSnapshot",
-    "SimpleCacheORM",
-    "PrimitiveSchemaResolver",
-    "SchemaResolver",
-    "SchemaKey",
-    "SlugProvider",
 ]
-
-def __getattr__(name: str):
-    # Lazy import to avoid circular import during CachedFileFolders module initialization
-    if name == "SimpleCacheORM":
-        from .simple_cache_orm import SimpleCacheORM  # type: ignore
-        return SimpleCacheORM
-    if name == "PrimitiveSchemaResolver":
-        from .primitive_schema_resolver import PrimitiveSchemaResolver  # type: ignore
-        return PrimitiveSchemaResolver
-    if name == "SlugProvider":
-        from .primitive_schema_protocol import SlugProvider  # type: ignore
-        return SlugProvider
-    if name == "SchemaResolver":
-        from .schema_resolver_protocol import SchemaResolver  # type: ignore
-        return SchemaResolver
-    if name == "SchemaKey":
-        from .schema_resolver_protocol import SchemaKey  # type: ignore
-        return SchemaKey
-    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
-
-
